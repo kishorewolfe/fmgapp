3913bd7cf169fed036211c7da970405ccfae3c0b
kishorewolfe <kishorewolfeik@gmail.com>
2024-10-12 00:32:59 +0530

    SEO Change

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 // next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://www.mywebsite.com',
+    siteUrl: 'https://www.myfamilymg.com',
     generateRobotsTxt: true, // (optional) Generate robots.txt file
     // ...other options
   }
