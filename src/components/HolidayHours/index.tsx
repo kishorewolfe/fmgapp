e6fea116300a140260f1c5f99f451a3a07ccb8e4
kishorewolfe <kishorewolf28@gmail.com>
2026-06-11 00:28:59 +0530

    Major Change 8.1

--- src/components/HolidayHours/index.tsx
+++ src/components/HolidayHours/index.tsx
@@ -9,26 +9,25 @@ type Props = {}
 // date: "03/29/2024 ",
 // day: "Friday",
 
-const HolidayHours = ({feature}: any) => {
-  const { name, blockDate,blockMonth,date,day } = feature;
+const HolidayHours = ({ feature }: any) => {
+  const { name, blockDate, blockMonth } = feature;
+
   return (
-    <div >  
-      <div className="p-2 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 dark:bg-gray-dark dark:text-white" >
-    <div className=" flex items-center">
-        <div className="holidaybtn">
-        <p className="text-4xl font-bold text-white">{blockDate}</p>
-        <p className="text-sm text-white">{blockMonth}</p>
+    <div>
+      <div className="p-2 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 dark:bg-gray-dark dark:text-white">
+        <div className="flex items-center">
+          <div className="holidaybtn">
+            <p className="text-sm  text-white">{blockDate}</p>
+            <p className="text-md font-semibold text-white">{blockMonth}</p>
+          </div>
+
+          <div className="ml-4">
+            <p className="text-lg font-semibold">{name}</p>
+          </div>
         </div>
-        <div className="ml-4">
-        <div className="uppercase tracking-wide text-sm text-lime-600 font-semibold">{day}</div>
-        <p className="mt-2 text-gray-500">{name}</p>
-        <p className="mt-2 text-gray-500">{date}</p>
-        </div>
+      </div>
     </div>
-    </div></div>
+  );
+};
 
-    
-  )
-}
-
-export default HolidayHours
+export default HolidayHours;
